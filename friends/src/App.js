11e6b49ad477b682fbc148633dd3fd01870c753e
Kurt Van Etten <kurt@vanetten.us>
2019-11-01 22:31:35 -0400

    basic App setup

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, withRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const AppWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	margin-top: 30px;
+
+	h1 {
+		font-size: 4rem;
+	}
+
+`;
+
+
+const App = () => {
+
+
+	return (
+		<h1>Auth-Friends</h1>
+	);
+};
+
 
 export default App;
